edd6624f14bffaf9c7969c661b94605b54833056
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-04-16 01:03:31 +0000

    [Add]Bookers2.1-2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "b186388595054740979ad3cb4a4b26d4.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
+
 end
