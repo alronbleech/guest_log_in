268660d652ecb24b8a11e5fa0fa1271f708f0ba1
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-04-17 14:49:46 +0000

    [Add]Bookers2.3-7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User,find(params[:id])
+    @books = @user.books
   end
 
   def edit
   end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
 end
