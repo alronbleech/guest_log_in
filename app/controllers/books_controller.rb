7ec593eb4d274cfbca74308451863a655d39e563
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-04-19 02:36:45 +0000

    [Fix]Bookers2.3-7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,22 +7,32 @@ class BooksController < ApplicationController
   end
 
   def show
+    @books = Book.find(params[:id])
+    @user = @books.user
+    @book = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to '/books'
+    redirect_to book_path(@book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
   end
 
   def show
-    @user = User,find(params[:id])
+    @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to books_path
   end
   
   private
