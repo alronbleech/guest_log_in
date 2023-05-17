4c94987fd86b5525654df92775d74856a05939ca
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-05-17 14:18:37 +0000

    [Add]guest_log_in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
   def index
     @user = current_user
     @users = User.all
@@ -41,4 +42,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user), notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books, only: [:show, :index, :edit, :create, :destroy, :update]
   resources :users, only: [:show, :index, :edit, :update]
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
